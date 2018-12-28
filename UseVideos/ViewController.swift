1e5042dad3f45f184dd32958c1fdfbbdc59df632
dmorenoar <dmorenoar@dmorenoars-mac.local>
2018-12-28 13:08:43 -0800

    Initial Commit

---
+++ UseVideos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UseVideos
+//
+//  Created by dmorenoar on 28/12/18.
+//  Copyright © 2018 dmorenoar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
